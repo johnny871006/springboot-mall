f22b03b72f1c0610d2856a02cc9e59a9caa01893
johnny <johnny871006@gmail.com>
2024-07-10 14:11:12 +0800

    設定返回商品的時間格式和時區

--- src/main/java/org/example/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/org/example/springbootmall/dao/impl/ProductDaoImpl.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 @Component
 public class ProductDaoImpl implements ProductDao {
