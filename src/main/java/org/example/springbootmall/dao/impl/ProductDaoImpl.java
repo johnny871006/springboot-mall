addf81a0fbdd4f4fc0c464313f54175ef5f2d591
johnny <johnny871006@gmail.com>
2024-07-16 11:17:50 +0800

    訂單功能 - 檢查User是否存在、扣除商品庫存

--- src/main/java/org/example/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/org/example/springbootmall/dao/impl/ProductDaoImpl.java
@@ -134,6 +134,21 @@ public class ProductDaoImpl implements ProductDao {
     }
 
     @Override
+    public void updateStock(Integer productId, Integer stock) {
+
+        String sql = "UPDATE products SET stock = :stock, last_modified_date = :lastModifiedDate " +
+                " WHERE product_id = :productId ";
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("productId", productId);
+
+        map.put("stock", stock);
+        map.put("lastModifiedDate", new Date());
+
+        namedParameterJdbcTemplate.update(sql,map);
+    }
+
+    @Override
     public void deleteProduct(Integer productId) {
 
         String sql = "DELETE FROM products WHERE product_id = :productId";
--- src/main/java/org/example/springbootmall/service/impl/OrderServiceImpl.java
+++ src/main/java/org/example/springbootmall/service/impl/OrderServiceImpl.java
@@ -2,28 +2,38 @@ package org.example.springbootmall.service.impl;
 
 import org.example.springbootmall.dao.OrderDao;
 import org.example.springbootmall.dao.ProductDao;
+import org.example.springbootmall.dao.UserDao;
 import org.example.springbootmall.dto.BuyItem;
 import org.example.springbootmall.dto.CreateOrderRequest;
 import org.example.springbootmall.model.Order;
 import org.example.springbootmall.model.OrderItem;
 import org.example.springbootmall.model.Product;
+import org.example.springbootmall.model.User;
 import org.example.springbootmall.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class OrderServiceImpl implements OrderService {
+
+    private final static Logger logger = LoggerFactory.getLogger(OrderServiceImpl.class);
 
     @Autowired
     private OrderDao orderDao;
 
     @Autowired
     private ProductDao productDao;
+
+    @Autowired
+    private UserDao userDao;
 
     @Override
     public Order getOrderById(Integer orderId) {
@@ -41,6 +51,14 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
 
+        // 檢查User是否存在
+        User user = userDao.getUserById(userId);
+
+        if(user == null){
+            logger.warn("此userId {} 沒有註冊，請先註冊!",userId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
         // 計算價錢
         int totalAmount = 0;
 
@@ -49,6 +67,16 @@ public class OrderServiceImpl implements OrderService {
 
         for(BuyItem buyItem : createOrderRequest.getBuyItemList()){
             Product product = productDao.getByProductId(buyItem.getProductId());
+
+            if(product == null){
+                logger.warn("沒有此商品Id {}",buyItem.getProductId());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }else if ( product.getStock() < buyItem.getQuantity()){
+                logger.warn("此商品 {} 庫存不足! ", product.getProductName());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+
+            productDao.updateStock(product.getProductId(),product.getStock() - buyItem.getQuantity());
 
             int amount = product.getPrice() * buyItem.getQuantity();
 
